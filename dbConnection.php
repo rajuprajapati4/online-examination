06623cc0e8dac5332b9e0f5fe1241d5d1a4cf45f
Raju Prajapati <48856985+rajuprajapati4@users.noreply.github.com>
2020-03-20 19:33:56 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,14 @@
 <?php
-//all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$servername = "remotemysql.com";
+$username = "Bl3Jqd1rm3";
+$password = "kn9xezFoyL";
+$db = "Bl3Jqd1rm3";
 
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
